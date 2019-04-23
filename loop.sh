ba37357896588b50f4883314780051dc2323e596
sherlockliu <hancock.liu@gmail.com>
2019-04-23 13:24:31 +0800

    Initial commit

---
+++ loop.sh
@@ -0,0 +1,4 @@
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
+
---
+++ variable.sh
@@ -0,0 +1,7 @@
+your_name="tom"
+echo $your_name
+your_name="alibaba"
+echo $your_name
+readonly myUrl="http://www.google.com"
+echo $myUrl
+
